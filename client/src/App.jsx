e23b70868123fada7772afdf67cd07cad8db1715
NyambaneA <nyambanealfred1539@gmail.com>
2023-11-28 23:11:02 +0300

    created land pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
+import Land from './pages/Land';
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
+        <Route path='/land' element={<Land />} />
         <Route path='/search' element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing />} />
 
