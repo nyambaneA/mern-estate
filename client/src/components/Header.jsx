799a584ffdb5f6431dc50a3e435927261bfb7423
NyambaneA <nyambanealfred1539@gmail.com>
2023-11-29 11:39:28 +0300

    created land component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -57,6 +57,11 @@ export default function Header() {
               About
             </li>
           </Link>
+          <Link to='/land'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              Land
+            </li>
+          </Link>
           <Link to='/profile'>
             {currentUser ? (
               <img
